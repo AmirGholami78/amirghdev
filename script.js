71cb5348a2820b2121bde1aedb54ce557c7bb2b0
Amirgh <144719295+AmirGholami78@users.noreply.github.com>
2023-11-28 22:46:50 +0330

    Add files via upload
    amirghdev V2.0

---
+++ script.js
@@ -0,0 +1,8 @@
+var popup = document.getElementById("popup")
+
+function openPopup() {
+    popup.classList.add("open-contact-popup")
+}
+function closePopup() {
+    popup.classList.remove("open-contact-popup")
+}
